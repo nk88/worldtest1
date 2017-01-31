16d071005037687f01de754649a6d28b335cb9d8
nk88 <nizank@gmail.com>
2017-01-31 13:47:40 +0200

    Update views.py

--- olam_aher/app/views.py
+++ olam_aher/app/views.py
@@ -8,32 +8,21 @@ import db
 def search_kb(request,id):
     results = db.search_file_name(id)
     colored_state = lambda state : HTML.TableCell(state, bgcolor='lime' if state == 'ProcessCompleted' else 'red')
-    table_data = [[row[0], row[1], colored_state(row[2]), row[7], row[3], HTML.link('Download', row[4]), \
+    table_data = [[row[0], row[1], colored_state(row[2]), HTML.link(row[7],'search_extracted/'+str(row[0])+'/'), row[3], HTML.link('Download', row[4]), \
                  HTML.link('Open', 'file:///' + row[5].replace('\\','/')), "{:%d/%m/%y}".format(row[6])] for row in results]
     htmlcode = HTML.table(table_data, header_row=['Id','File Name','State','#Extracted','Status','Url','Local Path','Retrieval Date'])	
-    """
-    table_data = [
-        #[id,       'success'],
-        [id, HTML.TableCell('success', bgcolor='lime'), HTML.link('Download', 'http://www.google.com/'), HTML.link('Open dir', 'file:///C:/Windows/')],
-    ]
-    result_colors = {
-        'success':      'lime',
-        'failure':      'red',
-        'error':        'yellow',
-    }
-    htmlcode = HTML.Table(header_row=['ID', 'Status'])
-    for row in table_data:
-        # create the colored cell:
-        colored_result = HTML.TableCell(row[1], bgcolor=result_colors[row[1]])
-        # append the row with two cells:
-        htmlcode.rows.append([row[0], colored_result])	
-    #return HttpResponse("stam " + id)
-    """
     return HttpResponse(htmlcode)
+
+@csrf_exempt
+def search_extracted(request,id):
+    results = db.search_extracted(id)
+    table_data = [row[0] for row in results]
+    return HttpResponse('<br/>'.join(table_data))
 	
 @csrf_exempt
 def get_page(request):
     url = request.body
     content = urllib2.urlopen(url).read()
     return HttpResponse(content)
+
 
