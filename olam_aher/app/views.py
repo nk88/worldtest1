4bf2655959640e9d76769d11f2f26a243a4b8f6c
nk88 <nizank@gmail.com>
2017-02-12 14:25:11 +0200

    Update views.py

--- olam_aher/app/views.py
+++ olam_aher/app/views.py
@@ -12,9 +12,11 @@ def search_kb(request,id):
     results = db.search_file_name(id) #[[123,'sdfsdhdff','ProcessCompleted','setet','http://www.google.com','c:\\\\',datetime.datetime.now(),4]]
     if len(results) == 0:
         htmlcode = HTML.table([[HTML.TableCell('Not Found', bgcolor='red')]])
+        htmlcode = '<h3 id="tbl'+str(id)+'">'+str(id)+'</h3>'+htmlcode	
         return HttpResponse(htmlcode)
     colored_state = lambda state : '<span style="background-color: %s">%s</span>' % ('lime' if state == 'ProcessCompleted' else 'red', state)
-    table_data = [[row[0], row[1], colored_state(row[2]), HTML.link(row[7],'search_extracted/'+str(row[0])+'/'), row[3], HTML.link('Download', row[4]), \
+    table_data = [[row[0], row[1], colored_state(row[2]), HTML.link('<span style="background-color: %s">%s</span>' % ('yellow' if str(row[7]) == '0' else 'white', row[7]) ,\
+	             'search_extracted/'+str(row[0])+'/'), row[3], HTML.link('Download', row[4]), \
                  HTML.link('Open', 'file:///' + row[5].replace('\\','/')), "{:%d/%m/%y}".format(row[6])] for row in results]
     htmlcode = HTML.table(table_data, header_row=['Id','File Name','State','#Extracted','Status','Url','Local Path','Retrieval Date'],attribs={'class': 'resultsTable'})
     htmlcode = '<h3 id="tbl'+str(id)+'">'+str(id)+'</h3>'+htmlcode
